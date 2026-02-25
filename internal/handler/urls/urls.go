af575d68421a75a3faf482b82b79b465066e9697
artni96 <artni96@yandex.ru>
2026-02-25 20:37:57 +0300

    testDB added

--- internal/handler/urls/urls.go
+++ internal/handler/urls/urls.go
@@ -1,11 +1,11 @@
 package urls
 
 import (
+	"io"
 	"net/http"
 )
 
-type postResponse struct {
-}
+var testDB = map[string]string{}
 
 func CreateURL(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
@@ -13,9 +13,24 @@ func CreateURL(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Method not allowed"))
 		return
 	}
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	testDB["/EwHXdJfB"] = string(body)
+
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte("Created"))
+	w.Write([]byte("http://localhost:8080/EWHXdJfB"))
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(r.Body)
 }
 
 func GetURL(w http.ResponseWriter, r *http.Request) {
@@ -24,7 +39,8 @@ func GetURL(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Method not allowed"))
 		return
 	}
+	redirectTo := testDB[r.URL.Path]
 
-	w.Header().Set("Location", "https://practicum.yandex.ru/ ")
+	w.Header().Set("Location", redirectTo)
 	w.WriteHeader(http.StatusTemporaryRedirect)
 }
