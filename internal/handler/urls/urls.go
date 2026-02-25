488d1f69ffe01c36903a7483782670e0f62b1df4
artni96 <artni96@yandex.ru>
2026-02-25 20:12:20 +0300

    not allowed method status changed just to 400

--- internal/handler/urls/urls.go
+++ internal/handler/urls/urls.go
@@ -9,20 +9,22 @@ type postResponse struct {
 
 func CreateURL(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("Method not allowed"))
+		return
 	}
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Created"))
 }
 
 func GetURL(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
-		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("Method not allowed"))
 		return
 	}
 
-	w.Header().Set("Location", "https://ya.ru/")
+	w.Header().Set("Location", "https://practicum.yandex.ru/ ")
 	w.WriteHeader(http.StatusTemporaryRedirect)
 }
