a143ccc6342b2cafaf1146d1a4d926eb0f8f1c5f
artni96 <artni96@yandex.ru>
2026-03-08 17:45:06 +0300

    generateID moved to utility

--- internal/utility/urls.go
+++ internal/utility/urls.go
@@ -10,7 +10,7 @@ func GenerateID(length int) (string, error) {
 	bytes := make([]byte, length)
 	_, err := rand.Read(bytes)
 	if err != nil {
-		return "", fmt.Errorf("Данный urlID уже используется")
+		return "", fmt.Errorf("данный urlID уже используется")
 	}
 	resp := base64.URLEncoding.EncodeToString(bytes)[:length]
 	return resp, err
