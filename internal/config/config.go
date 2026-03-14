62548d3eb35770298685549bac3688734e22e150
artni96 <artni96@yandex.ru>
2026-03-14 11:57:27 +0300

    env vars added

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,15 +6,23 @@ import (
 )
 
 type Config struct {
-	ServerAddress string
-	ResponseURL   string
+	ServerAddress string `env:"SERVER_ADDRESS"`
+	ResponseURL   string `env:"BASE_URL"`
 }
 
 func ParseFlags() (*Config, error) {
 	fs := flag.NewFlagSet("fs", flag.ExitOnError)
 	conf := Config{}
-	fs.StringVar(&conf.ServerAddress, "a", "localhost:8080", "server address")
-	fs.StringVar(&conf.ResponseURL, "b", "http://localhost:8080", "response URL")
+	if envServerAddress := os.Getenv("SERVER_ADDRESS"); envServerAddress != "" {
+		conf.ServerAddress = envServerAddress
+	} else {
+		fs.StringVar(&conf.ServerAddress, "a", "localhost:8080", "server address")
+	}
+	if envBaseURL := os.Getenv("BASE_URL"); envBaseURL != "" {
+		conf.ResponseURL = envBaseURL
+	} else {
+		fs.StringVar(&conf.ResponseURL, "b", "http://localhost:8080", "response URL")
+	}
 	err := fs.Parse(os.Args[1:])
 	if err != nil {
 		return nil, err
