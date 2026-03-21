251529edc261ad527700ba74c777321027e006b1
artni96 <artni96@yandex.ru>
2026-03-21 12:26:57 +0300

    logs fileout added

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -2,9 +2,11 @@ package logger
 
 import (
 	"net/http"
+	"os"
 	"time"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var Logger *zap.SugaredLogger = zap.NewNop().Sugar()
@@ -33,16 +35,28 @@ func (r *loggingResponseWriter) WriteHeader(status int) {
 }
 
 func InitLogger(level string) error {
-	lvl, err := zap.ParseAtomicLevel(level)
+	levelMap := map[string]zapcore.Level{
+		"debug": zapcore.DebugLevel,
+		"info":  zapcore.InfoLevel,
+		"warn":  zapcore.WarnLevel,
+		"error": zapcore.ErrorLevel,
+		"fatal": zapcore.FatalLevel,
+	}
+
+	consoleEncoder := zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
+	logFileConfig := zap.NewDevelopmentEncoderConfig()
+	fileEncoder := zapcore.NewJSONEncoder(logFileConfig)
+
+	logFile, err := os.OpenFile("./internal/logger/shortener.log", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
 		return err
 	}
-	cfg := zap.NewDevelopmentConfig()
-	cfg.Level = lvl
-	logger, err := cfg.Build()
-	if err != nil {
-		return err
-	}
+
+	fileOut := zapcore.NewCore(fileEncoder, zapcore.AddSync(logFile), levelMap[level])
+	stdOut := zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), levelMap[level])
+
+	loggerCore := zapcore.NewTee(fileOut, stdOut)
+	logger := zap.New(loggerCore, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
 	sugar := logger.Sugar()
 	Logger = sugar
 	return nil
@@ -71,6 +85,7 @@ func RequestLogger(h http.Handler) http.Handler {
 			"response_status", responseData.status,
 			"response_size", responseData.size,
 		)
+
 	}
 	return http.HandlerFunc(logFn)
 }
