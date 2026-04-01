b4949a3d3d1ac1cc727107739b17fffce4cc2cb6
artni96 <artni96@yandex.ru>
2026-04-01 17:24:02 +0300

    dbAddressToRunMigrations added

--- internal/config/db/db_config.go
+++ internal/config/db/db_config.go
@@ -4,12 +4,23 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/artni96/url-shortener/internal/config"
 	"go.uber.org/zap"
 )
+
+type dBConnectionSettings struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+}
 
 func InitDBConnection(ctx context.Context, cfg *config.Config, log *zap.Logger) (*sql.DB, error) {
 	localCtx, cancel := context.WithTimeout(ctx, 3*time.Second)
@@ -40,27 +51,47 @@ func InitDBConnection(ctx context.Context, cfg *config.Config, log *zap.Logger) 
 		return nil, err
 	}
 
-	//if err := runMigrations(); err != nil {
-	//	log.Info("failed to run migrations",
-	//		zap.String("error message", err.Error()),
-	//	)
-	//	return nil, err
-	//}
-	//
-	//log.Info("Migrations completed successfully")
+	if err := runMigrations(cfg); err != nil {
+		log.Info("failed to run migrations",
+			zap.String("error message", err.Error()),
+		)
+		return nil, err
+	}
+
+	log.Info("Migrations completed successfully")
 	return db, nil
 }
 
-func runMigrations() error {
-
+func runMigrations(cfg *config.Config) error {
+	dbAddress, err := dbAddressToRunMigrations(cfg)
+	if err != nil {
+		return err
+	}
+	fmt.Println("db address:", dbAddress)
 	cmd := exec.Command(
 		"migrate",
-		"-database", "postgres://postgres:postgres@localhost:5432/url_shortener?sslmode=disable",
+		"-database", dbAddress,
 		"-path", "./migrations", "up",
 	)
-	_, err := cmd.CombinedOutput()
+	_, err = cmd.CombinedOutput()
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func dbAddressToRunMigrations(cfg *config.Config) (string, error) {
+	dbConnParams := make(map[string]string)
+	splitDatabaseDsn := strings.Split(cfg.DatabaseDsn, " ")
+	for _, s := range splitDatabaseDsn {
+		if s != "" {
+			splitObj := strings.Split(s, "=")
+			dbConnParams[splitObj[0]] = splitObj[1]
+		}
+	}
+	if len(dbConnParams) != 6 {
+		return "", fmt.Errorf("database dsn is not valid")
+	}
+	result := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s", dbConnParams["user"], dbConnParams["password"], dbConnParams["host"], dbConnParams["port"], dbConnParams["dbname"], dbConnParams["sslmode"])
+	return result, nil
 }
