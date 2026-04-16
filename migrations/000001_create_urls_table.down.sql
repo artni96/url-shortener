609ec16f6e55e1f85f477b4f2afadc0897b9af0e
artni96 <artni96@yandex.ru>
2026-04-16 16:33:49 +0300

    down revision fixed

--- migrations/000001_create_urls_table.down.sql
+++ migrations/000001_create_urls_table.down.sql
@@ -1,7 +1,7 @@
 DROP INDEX IF EXISTS idx_short_url;
 DROP INDEX IF EXISTS idx_original_url_unique;
-DROP TABLE urls;
+DROP TABLE urls CASCADE;
 
 DROP INDEX IF EXISTS idx_user_id;
 DROP INDEX IF EXISTS idx_username_unique;
-DROP TABLE users;
+DROP TABLE users CASCADE;
