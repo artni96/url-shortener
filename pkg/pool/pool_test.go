e00b277cc48f8bd7a7d181bac871b58d966c6afa
artni96 <artni96@yandex.ru>
2026-06-25 13:54:29 +0300

    tests for pool added

--- pkg/pool/pool_test.go
+++ pkg/pool/pool_test.go
@@ -1,53 +1,49 @@
 package pool
 
-//func TestNew(t *testing.T) {
-//	tests := []struct {
-//		name        string
-//		factory     func() testBuffer
-//		expectError bool
-//	}{
-//		{
-//			name: "valid factory function",
-//			factory: func() testBuffer {
-//				return testBuffer{data: make([]byte, 0, 1024)}
-//			},
-//			expectError: false,
-//		},
-//		{
-//			name:        "nil factory function",
-//			factory:     nil,
-//			expectError: true,
-//		},
-//	}
-//
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			pool, err := New(tt.factory)
-//
-//			if tt.expectError {
-//				if err == nil {
-//					t.Error("Expected error but got nil")
-//				}
-//				if !errors.Is(err, ErrFuncNil) {
-//					t.Errorf("Expected ErrFuncNil, got %v", err)
-//				}
-//				if pool != nil {
-//					t.Error("Expected nil pool when error occurs, got non-nil")
-//				}
-//			} else {
-//				if err != nil {
-//					t.Errorf("Unexpected error: %v", err)
-//				}
-//				if pool == nil {
-//					t.Error("Expected non-nil pool, got nil")
-//				}
-//				if pool.pool == nil {
-//					t.Error("Expected non-nil sync.Pool, got nil")
-//				}
-//				if pool.pool.New == nil {
-//					t.Error("Expected New function to be set, got nil")
-//				}
-//			}
-//		})
-//	}
-//}
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type testStruct struct {
+	field string
+}
+
+func (t *testStruct) Reset() {
+	t.field = ""
+}
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		name      string
+		factory   func() *testStruct
+		expectErr bool
+	}{
+		{
+			name: "success",
+			factory: func() *testStruct {
+				return &testStruct{field: "test"}
+			},
+			expectErr: false,
+		},
+		{
+			name:      "failure - nil factory function returns error",
+			factory:   nil,
+			expectErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := New(tt.factory)
+
+			if tt.expectErr {
+				assert.Error(t, err)
+				assert.ErrorIs(t, err, ErrFuncNil)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
